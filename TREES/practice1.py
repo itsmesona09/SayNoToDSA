18efbedd9446967516ffeef46376b47838571609
sona-devassure <sona@devassure.io>
2025-08-10 18:23:19 +0530

    Added all traversal techniques

--- TREES/practice1.py
+++ TREES/practice1.py
@@ -29,6 +29,26 @@ class Tree:
             [root.val] +
             self.in_order(root.right)
         )
+        
+    def pre_order(self, root):
+        if not root:
+            return []
+
+        return (
+            [root.val] + 
+            self.pre_order(root.left) +
+            self.pre_order(root.right)
+        )
+        
+    def post_order(self, root):
+        if not root:
+            return []
+        
+        return (
+            self.post_order(root.left) + 
+            self.post_order(root.right) +
+            [root.val]
+        )
 
 # create an instance for TreeNode Class which has init method and creates a tree (left and right and val)
 # give a value of 1 as root node
@@ -59,4 +79,6 @@ tree = Tree()
 #     TreeNode right
 # }
 
-print(tree.in_order(root))
+print("IN-ORDER: ", tree.in_order(root))
+print("PRE-ORDER: ",tree.pre_order(root))
+print("POST-ORDER: ",tree.post_order(root))
