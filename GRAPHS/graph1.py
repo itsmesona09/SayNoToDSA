21f2a6d2e7fd39d9d0916857082f57b3d024bd64
sona-devassure <sona@devassure.io>
2025-08-17 10:47:10 +0530

    initialized a empty dictionary to store graphs rel data

---
+++ GRAPHS/graph1.py
@@ -0,0 +1,7 @@
+class Graph:
+    def __init__(self):
+        # initializes an empty dictionary graph
+        # start of a class representing a graph data structure
+        
+        # prepare the instance to store a graph related data (adjacency lists or nodes)
+        self.graph = {}
