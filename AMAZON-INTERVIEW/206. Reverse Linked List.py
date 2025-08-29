cc88bef0d36441687dcbcc6c912dae574c967d9c
sona-devassure <sona@devassure.io>
2025-08-29 06:12:56 +0530

    206. Reverse Linked List

---
+++ AMAZON-INTERVIEW/206. Reverse Linked List.py
@@ -0,0 +1,35 @@
+class Solution:
+    def reverseList(self, head):
+        stack = []
+        curr = head
+        
+        while curr:
+            stack.append(curr)
+            curr = curr.next
+            
+        if not stack:
+            return None
+        
+        newhead = stack.pop()
+        curr = newhead
+        while stack:
+            curr.next = stack.pop()
+            curr = curr.next
+        
+        curr.next = None
+        
+        return newhead
+            
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next    
+        
+rev = Solution()
+head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+newhead = rev.reverseList(head)
+curr = newhead
+
+while curr:
+    print(curr.val, end = '->' if curr.next else '\n')
+    curr = curr.next
